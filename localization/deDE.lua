7993f4421a0c64fef791834206b5f18ca06c8849
Kruithne <kruithne@gmail.com>
2016-08-16 19:15:11 +0100

    Correct deDE localization since recent changes

--- localization/deDE.lua
+++ localization/deDE.lua
@@ -2,9 +2,9 @@ local L = TransmogTokens.L
 
 if GetLocale() == "deDE" then
 	L["Loading item information..."] = "Lade Gegenstandsinformationen...";
-	L["Can be exchanged for %d appearances you need."] = "Du benötigst %d Vorlagen von dieser Rüstungsmarke.";
-	L["Can be exchanged for 1 appearance you need."] = "Du benötigst 1 Vorlage von dieser Rüstungsmarke.";
+	L["Can be exchanged for %d appearances you need."] = "Kann gegen %d dir fehlende Vorlagen getauscht werden.";
+	L["Can be exchanged for 1 appearance you need."] = "Kann gegen 1 dir fehlende Vorlagen getauscht werden.";
 	L["Appearances from this are class and/or spec dependant."] = "Vorlagen von diesem Gegenstand sind abhängig von Klasse und/oder Spezialisierung.";
-	L["Can be exchanged for appearances you don't need."] = "Du benötigst keine Vorlagen von dieser Rüstungsmarke.";
+	L["Can be exchanged for appearances you don't need."] = "Kann gegen dir nicht fehlende Vorlagen getauscht werden.";
 	L["This token cannot be redeemed for anything."] = "Diese Rüstungsmarke kann gegen nichts eingetauscht werden."
 end

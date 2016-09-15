1fd08dc2306fa875121b3b68dd5c4aa43debb4d1
ChihChung Ma <mccjohnny@gmail.com>
2016-09-16 00:52:35 +0800

    Add translation for the zhTW locale

---
+++ localization/zhTW.lua
@@ -0,0 +1,10 @@
+local L = TransmogTokens.L
+
+if GetLocale() == "zhTW" then
+	L["Loading item information..."] = "讀取物品資訊中...";
+	L["Can be exchanged for %d appearances you need."] = "你需要的 %d 個外觀可被兌換。";
+	L["Can be exchanged for 1 appearance you need."] = "你需要的 1 個外觀可被兌換。";
+	L["Appearances from this are class and/or spec dependant."] = "此外觀需要特定職業或天賦。";
+	L["Can be exchanged for appearances you don't need."] = "可以兌換你不需要的外觀。";
+	L["This token cannot be redeemed for anything."] = "此徽章無法兌換任何物品。"
+end
